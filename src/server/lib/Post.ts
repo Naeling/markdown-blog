ca3d51b2e490655a62849882d0dcd648b6255911
Louis Groisne <louis.groisne@theodo.com>
2025-03-30 23:13:42 +0200

    Implement a Post lib file to get all posts

---
+++ src/server/lib/Post.ts
@@ -0,0 +1,24 @@
+import compact from "lodash/compact";
+
+import { Post } from "@/server/models";
+import { PostDTO, postSchema } from "@/lib/validation/post";
+
+export async function getAllPosts() {
+  const rawPosts = await Post.find().sort({ createdAt: -1 });
+  const posts: (PostDTO | null)[] = rawPosts.map((post) => {
+    const parsedPost = postSchema.safeParse({
+      id: post._id.toString(),
+      title: post.title,
+      content: post.content,
+      createdAt: post.createdAt,
+      updatedAt: post.updatedAt,
+    });
+    if (!parsedPost.success) {
+      console.log(`Found invalid post ${post}`);
+      return null;
+    }
+    return parsedPost.data;
+  });
+
+  return compact(posts);
+}
