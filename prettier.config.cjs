51b30eecce2e5c732447058678dad6c03784d7a9
Louis Groisne <louis.groisne@theodo.com>
2025-04-01 23:01:12 +0200

    Add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  bracketSameLine: false,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 80,
+  trailingComma: "es5",
+};
