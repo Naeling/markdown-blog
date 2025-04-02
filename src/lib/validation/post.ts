48858fef116b74b20484cdd2d67d720cf34167b8
Louis Groisne <louis.groisne@theodo.com>
2025-04-02 22:42:44 +0200

    Add pnpm install step in lint and build wfw

--- src/lib/validation/post.ts
+++ src/lib/validation/post.ts
@@ -16,3 +16,5 @@ export const createPostSchema = postSchema.omit({
 
 export type PostDTO = z.infer<typeof postSchema>;
 export type CreatePostInput = z.infer<typeof createPostSchema>;
+
+console.log("Hello world");
