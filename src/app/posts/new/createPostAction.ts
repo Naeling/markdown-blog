0dbead1f6df0ca35fb7a6ca3df76135ca9cd193c
Louis Groisne <louis.groisne@theodo.com>
2025-04-12 13:17:58 +0200

    Redirect to /posts after form submit

--- src/app/posts/new/createPostAction.ts
+++ src/app/posts/new/createPostAction.ts
@@ -4,7 +4,10 @@ import { createPostSchema } from "@/modules/posts";
 import { createPost } from "@/modules/posts";
 import { formDataToObject } from "@/utils";
 
+import { redirect } from "next/navigation";
+
 export async function createPostAction(formData: FormData) {
   const createPostData = createPostSchema.parse(formDataToObject(formData));
   await createPost(createPostData);
+  redirect("/posts");
 }
