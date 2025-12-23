4436dd7fe3cbf38f9d79f6543c5d2c3e8736e0a1
Louis Groisne <louis.groisne@gmail.com>
2025-12-23 21:07:41 +0100

    feat(post): display no posts button

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,8 +1,17 @@
+import { CreateFirstPostButton } from "@/modules/posts/components/CreateFirstPostButton";
 import { PostCard } from "@/modules/posts/components/PostCard";
 import { getAllPosts } from "@/modules/posts/lib/Post";
 
 export default async function Posts() {
   const posts = await getAllPosts();
+  if (posts.length === 0) {
+    return (
+      <div>
+        <p className="mb-2">No post to display</p>
+        <CreateFirstPostButton />
+      </div>
+    );
+  }
   return (
     <ul>
       {posts.map((post) => (
